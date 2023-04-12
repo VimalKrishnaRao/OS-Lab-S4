66ed513ea2471125d75e42456ca13d7755efd05f
VimalKrishnaRao <96693740+VimalKrishnaRao@users.noreply.github.com>
2023-04-12 13:46:28 +0530

    Update PriorityScheduling.c

--- PriorityScheduling.c
+++ PriorityScheduling.c
@@ -1,29 +1,19 @@
 #include<stdio.h>
-int q[100];
-int f = -1;
-int r = -1;
-void insert (int n)
+void swap (int *a, int *b)
 {
-        if (f==-1)
-                f=0;
-        r = r+1;
-        q[r] = n;
-}
-int delete ()
-{
-        int n;
-        n = q[f];
-        f = f+1;
-        return n;
+        int temp;
+        temp = *a;
+        *a = *b;
+        *b = temp;
 }
 int main()
 {
-        int p, tq, n, i, t=0;
+        int i, j, n, cmpt;
         float twt = 0, ttat = 0;
         printf ("\nEnter how many processes: ");
         scanf ("%d", &n);
-        int a[n][5], bt[10], exist[10] = {0};
-        printf ("\n Enter arrival time & burst time of each process:");
+        int a[n][6];
+        printf ("\n Enter arrival time, burst time and priority of each process:");
         for (i=0; i<n; i++)
         {
                 
@@ -33,53 +23,55 @@ int main()
                 scanf ("%d", &a[i][1]);
                 printf ("\nB.T of process %d: ", a[i][0]);
                 scanf ("%d", &a[i][2]);
-                bt[i] = a[i][2];
+                printf ("\nPriority of process %d: ", a[i][0]);
+                scanf ("%d", &a[i][5]);
         }
-        printf ("\nEnter time quantum: ");
-        scanf ("%d", &tq);
-        insert (0);
-        exist[0] = 1;
-        while (f<=r)
-        {
-                p = delete ();
-                if (a[p][2]>=tq)
-                {
-                        a[p][2]-=tq;
-                        t+=tq;
-                }
-                else
-                {
-                        t+=a[p][2];
-                        a[p][2] = 0;
-                }
-                for (i=0; i<n; i++)
-                {
-                        if (exist[i]==0 && a[i][1]<=t)
-                        {
-                                insert (i);
-                                exist[i]=1;
-                        }
-                }
-                if (a[p][2]==0)
-                {
-                        a[p][4]=t-a[p][1];
-                        a[p][3]=a[p][4]-bt[p];
-                        ttat+=a[p][4];
-                        twt+=a[p][3];
-                }
-                else
-                {
-                        insert (p);
-                }
-        }        
-        printf("\nP.No.\tAT\tBT\tTAT\tWT");
         for (i=0; i<n; i++)
         {
-            printf("\n%d\t%d\t%d\t%d\t%d", a[i][0], a[i][1], bt[i], a[i][4], a[i][3]);
+            for (j=0; j<n-i-1; j++)
+            {
+                if (a[j][1]>a[j+1][1])
+                {
+                        swap (&a[j][0], &a[j+1][0]);    
+                        swap (&a[j][1], &a[j+1][1]); 
+                        swap (&a[j][2], &a[j+1][2]); 
+                        swap (&a[j][5], &a[j+1][5]);
+                }
+            }
+        }
+        a[0][3] = a[0][1];
+        a[0][4] = a[0][2] - a[0][1];
+        cmpt = a[0][4];
+        twt+=a[0][3];
+        ttat+=a[0][4];
+        for (i=1; i<n; i++)
+        {
+                int min = a[i][5]; 
+                for (j=i+1; j<n; j++)
+                {
+                        if (min>a[j][5] && a[j][1]<=cmpt)
+                        {
+                                min = a[j][5];
+                                swap (&a[i][0], &a[j][0]);
+                                swap (&a[i][1], &a[j][1]);
+                                swap (&a[i][2], &a[j][2]);
+                                swap (&a[i][5], &a[j][5]);
+                        }
+                }
+                a[i][3] = cmpt - a[i][1];
+                twt+=a[i][3];
+                cmpt+=a[i][2];
+                a[i][4] = cmpt - a[i][1];
+                ttat+=a[i][4];
+        }
+        printf("\nP.No.\tAT\tBT\tP.R\tTAT\tWT");
+        for (i=0; i<n; i++)
+        {
+            printf("\n%d\t%d\t%d\t%d\t%d\t%d", a[i][0], a[i][1], a[i][2], a[i][5], a[i][4], a[i][3]);
         }
         printf ("\nAverage Turnaround time = %f", ttat/n);
         printf ("\nAverage Waiting time = %f", twt/n);
         return 0;
-} 
-
+}
+                
                 
